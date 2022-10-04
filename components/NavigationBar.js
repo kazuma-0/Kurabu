b25a718594f9d70046454831ced6f10bec12979b
kazuma-0 <as8644459@gmail.com>
2022-10-04 20:19:21 +0530

    Rename dashboard to login and added href on image

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -16,15 +16,21 @@ const links = [
         label: "blog",
         url: "/blog"
     },
+    {
+        label: "login",
+        url: "/auth"
+    }
 
 ]
 
 
 function NavigationBar(){
     return <Container maxW={"container.xl"} className={"h-28 flex justify-between items-center"}>
-        <div>
-            <img src={metaverse.src} width={"100px"} alt=""/>
-        </div>
+        <Link href={'/'}>
+            <div className={"cursor-pointer"}>
+                <img src={metaverse.src} width={"100px"} alt=""/>
+            </div>
+        </Link>
         <div className="flex space-x-8">
             {
                 links.map(link=>{
