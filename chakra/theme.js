c91f60c9ffd2f7811ae356ef7b89df4e80ae1dea
kazuma-0 <as8644459@gmail.com>
2022-10-05 14:38:24 +0530

    change text color

--- chakra/theme.js
+++ chakra/theme.js
@@ -4,35 +4,32 @@ import { extendTheme } from '@chakra-ui/react';
 const styles = {
     global: props => ({
         body: {
-            color: mode('#f4f1d0', '#f4f1d0')(props),
+            color: mode('#f4f1d0', '#fdfcf5')(props),
             bg: mode('#101014', '#101014')(props),
         },
     }),
 };
 
 const components = {
-    Drawer: {
-        // setup light/dark mode component defaults
-        baseStyle: props => ({
-            dialog: {
-                bg: mode('white', '#141214')(props),
-            },
-        }),
-    },
-    Alert: {
-        variants:{
-            solid:{
-                bg:"black"
-            },
-            leftAccent:{
-                bg:"black !important"
-            }
-        }
-
-    },
-    Toast:{
-
-    }
+//     Drawer: {
+//         // setup light/dark mode component defaults
+//         baseStyle: props => ({
+//             dialog: {
+//                 bg: mode('white', '#141214')(props),
+//             },
+//         }),
+//     },
+//     Alert: {
+//         variants:{
+//             solid:{
+//                 bg:"black"
+//             },
+//             leftAccent:{
+//                 bg:"black !important"
+//             }
+//         }
+//
+//     },
 };
 
 const theme = extendTheme({
