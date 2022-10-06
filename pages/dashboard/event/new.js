d89cce26e938285374febaba252a6c6f5285d048
kazuma-0 <as8644459@gmail.com>
2022-10-06 16:16:09 +0530

    Complete logic for new events

--- pages/dashboard/event/new.js
+++ pages/dashboard/event/new.js
@@ -1,32 +1,119 @@
-import {checkUser} from "../../../utils";
-import {useEffect, useState} from "react";
-import {useRouter} from "next/router";
-import {useToast} from "@chakra-ui/react";
-import {useWallet} from "@solana/wallet-adapter-react";
+import { checkUser } from "../../../utils";
+import { useCallback, useEffect, useReducer, useState } from "react";
+import { useRouter } from "next/router";
+import { useToast } from "@chakra-ui/react";
+import { useWallet } from "@solana/wallet-adapter-react";
 import EditorLayout from "../../../components/EditorLayout";
+import eventFormReducer from "../../../reducers/EventFormReducer";
+import {frontendClient} from "../../../client";
+import {IconLoader} from "@tabler/icons";
 
-function Blog(){
-    const toast = useToast({})
-    const router = useRouter()
-    const {connected, publicKey} = useWallet();
-    const [user, setUser] = useState(null);
-    const [css, setCss] = useState('')
-    const [markdown, setMarkdown] = useState('')
-    useEffect(() => {
-        if (!connected && publicKey) {
-            router.push('/auth')
-        }
-        if (publicKey !== null) {
-            checkUser(publicKey, setUser);
-        }
-    }, [publicKey])
+function NewEvent() {
+  const toast = useToast({
+    position:"bottom-right",
+    variant: "left-accent"
+  })
+  const router = useRouter();
+  const { connected, publicKey } = useWallet();
+  const [user, setUser] = useState(null);
+  const [css, setCss] = useState("");
+  const [markdown, setMarkdown] = useState("");
+  useEffect(() => {
+    if (!connected && publicKey) {
+      router.push("/auth");
+    }
+    if (publicKey !== null && user === null) {
+      checkUser(publicKey, setUser);
+    }
+    console.log(user);
 
-    return (
-        <div className={"h-cover"}>
-        <h1 className={"text-4xl uppercase font-bold font-ligurino tracking-wider pb-5"}>New post</h1>
-            <EditorLayout markdown={markdown} css={css}/>
-        </div>
-    )
+    if (user) {
+      dispatch({
+        type: "public key",
+        field: "pubKey",
+        payload: publicKey.toBase58(),
+      });
+      console.log(formState);
+      dispatch({
+        type: "author",
+        field: "author",
+        payload: user?.name,
+      });
+    }
+  }, [publicKey, user]);
+  const initialEventForm = {
+    title: "",
+    image_url: "",
+    tags: "",
+    description: "",
+    markdown: "",
+    styles: "",
+    author: "",
+    pubKey: "",
+  };
+  const [formState, dispatch] = useReducer(eventFormReducer, initialEventForm);
+
+  const dispatcher = useCallback((e) => {
+    console.log(e.target.value);
+    dispatch({
+      type: e.target.name,
+      field: e.target.name,
+      payload: e.target.value,
+    });
+  }, []);
+
+  const onChangeMarkdown = useCallback((e) => {
+    dispatch({
+      type: "markdown",
+      field: "markdown",
+      payload: e,
+    });
+  }, []);
+  const onChangeCss = useCallback((e) => {
+    dispatch({
+      type: "styles",
+      field: "styles",
+      payload: e,
+    });
+  }, []);
+
+  async function onSubmit(){
+    try{
+      const {data} = await frontendClient.post("/event/new",{
+        ...formState
+      })
+      toast({
+        title:"Post created successfully",
+        description: "Redirecting in 3 seconds.",
+        variant:"left-accent",
+        icon: <IconLoader/>
+      })
+    }catch (e){
+      toast({
+        title:"Failed to create post",
+        description: e.request.data.message,
+        status:"error"
+      })
+    }
+  }
+  return (
+    <div className={"-cover"}>
+      <h1
+        className={
+          "text-4xl uppercase font-bold font-ligurino tracking-wider pb-5"
+        }
+      >
+        New post
+      </h1>
+      <EditorLayout
+        onChangeMd={onChangeMarkdown}
+        onChangeCss={onChangeCss}
+        dispatcher={dispatcher}
+        state={formState}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
 }
 
-export default Blog;
+export default NewEvent;
