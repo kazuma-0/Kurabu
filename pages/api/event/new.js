23672248029e85c5f829593e07a104134cf7dd09
kazuma-0 <as8644459@gmail.com>
2022-10-06 16:16:25 +0530

    Api endpoint for new events

---
+++ pages/api/event/new.js
@@ -0,0 +1,22 @@
+// post request is used to prevent other people from just accessing this information by logging in.
+import { backendClient } from "../../../client";
+import {useToast} from "@chakra-ui/react";
+
+export default async function handler(req, res) {
+    if (req.method !== "POST") {
+        res.status(401).json("Method not allowed");
+        return;
+    }
+    try {
+        const {data} = await backendClient.post("/event/new", {
+            ...req.body
+        },{
+            headers:{
+                auth: req.body.pubKey
+            }
+        })
+        res.status(200).json(data);
+    } catch (e) {
+        res.status(401).json("User not in club");
+    }
+}
