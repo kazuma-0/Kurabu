c80395c7fa8f369abf4bab40702bb631c7174689
kazuma-0 <as8644459@gmail.com>
2022-10-06 16:01:46 +0530

    add api endpoint to validate invite

--- pages/api/validate-invite.js
+++ pages/api/validate-invite.js
@@ -1,16 +1,16 @@
-import {backendClient} from "../../client";
+import { backendClient } from "../../client";
 
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-        res.status(401).json("Method not allowed")
-        return;
-    }
-    try {
-        const {data, status} = await backendClient.post('/invite/validate', {
-            code: req.body.code
-        })
-        res.status(200).json(data);
-    } catch (e) {
-        res.status(401).json("Invalid code");
-    }
+  if (req.method !== "POST") {
+    res.status(401).json("Method not allowed");
+    return;
+  }
+  try {
+    const { data, status } = await backendClient.post("/invite/validate", {
+      code: req.body.code,
+    });
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(401).json("Invalid code");
+  }
 }
