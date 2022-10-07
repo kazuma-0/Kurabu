a847a2659673df2a3ee05bb4c222fd65a0d97219
kazuma-0 <as8644459@gmail.com>
2022-10-07 11:47:30 +0530

    refomat and logic changes

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,66 +1,68 @@
-import { Center, useToast } from "@chakra-ui/react";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import { useLocalStorage, useWallet } from "@solana/wallet-adapter-react";
-import { useCallback, useContext, useEffect } from "react";
-import { frontendClient } from "../../client";
-import { useRouter } from "next/router";
+import {Center, useToast} from "@chakra-ui/react";
+import {WalletMultiButton} from "@solana/wallet-adapter-react-ui";
+import {useWallet} from "@solana/wallet-adapter-react";
+import {useCallback, useEffect} from "react";
+import {frontendClient} from "../../client";
+import {useRouter} from "next/router";
 
 function Login() {
-  const { connected, publicKey } = useWallet();
-  const router = useRouter();
-  const [_, setUser] = useLocalStorage("user", null);
-  const toast = useToast({
-    position: "bottom-right",
-  });
-  async function onWalletConnected() {
-    try {
-      const { data } = await frontendClient.post("login", {
-        pubKey: publicKey,
-      });
-      // console.log(data)
-      // setUser(data);
-      toast({
-        title: "Successfully logged in",
-        description: "Redirecting to dashboard in 3s",
-        status: "success",
-        variant: "left-accent",
-        duration: 2e3,
-      });
-      setTimeout(() => {
-        router.push("/dashboard");
-      }, 3e3);
-    } catch (e) {
-      toast({
-        title: "User not in club.",
-        description:
-          "possess an invite code? register to continue. Redirecting in 3s",
-        variant: "left-accent",
-        isClosable: false,
-        status: "error",
-      });
-      onNoAccount();
+    const {connected, publicKey} = useWallet();
+    const router = useRouter();
+    const toast = useToast({
+        position: "bottom-right",
+    });
+
+    async function onWalletConnected() {
+        try {
+            const {data} = await frontendClient.post("login", {
+                pubKey: publicKey,
+            });
+            if (!toast.isActive(1)) {
+                toast({
+                    title: "Successfully logged in",
+                    description: "Redirecting to dashboard a second",
+                    status: "success",
+                    variant: "left-accent",
+                    duration: 1e3,
+                    id: 1
+                });
+            }
+            setTimeout(() => {
+                router.push("/dashboard");
+            }, 1e3);
+        } catch (e) {
+            toast({
+                title: "User not in club.",
+                description:
+                    "possess an invite code? register to continue. Redirecting in 3s",
+                variant: "left-accent",
+                isClosable: false,
+                status: "error",
+            });
+            onNoAccount();
+        }
     }
-  }
-  const onNoAccount = useCallback(() => {
-    setTimeout(() => {
-      router.push("/auth/register");
-    }, 3e3);
-  }, []);
-  useEffect(() => {
-    if (connected) {
-      onWalletConnected();
-    }
-  }, [connected]);
-  return (
-    <Center className={"h-cover"}>
-      <div className="flex flex-col items-center space-y-5">
-        <WalletMultiButton />
-        <div className={"text-center text-xl tracking-wide uppercase"}>
-          Connect your solana wallet to continue
-        </div>
-      </div>
-    </Center>
-  );
+
+    const onNoAccount = useCallback(() => {
+        setTimeout(() => {
+            router.push("/auth/register");
+        }, 3e3);
+    }, []);
+    useEffect(() => {
+        if (connected) {
+            onWalletConnected();
+        }
+    }, [connected]);
+    return (
+        <Center className={"h-cover"}>
+            <div className="flex flex-col items-center space-y-5">
+                <WalletMultiButton/>
+                <div className={"text-center text-xl tracking-wide uppercase"}>
+                    Connect your solana wallet to continue
+                </div>
+            </div>
+        </Center>
+    );
 }
 
 export default Login;
