bb3dd6007485cdc6cf6ad32ab79913bbd51a048d
kazuma-0 <as8644459@gmail.com>
2022-10-07 11:48:44 +0530

    seperated component for displaying all events

---
+++ components/EventViewer.js
@@ -0,0 +1,27 @@
+import {frontendClient} from "../client";
+import EventCard from "./EventCard";
+import {useEffect, useState} from "react";
+
+function EventViewer(props) {
+    const [events, setEvents] = useState(null)
+    useEffect(() => {
+        async function fetchEvents() {
+            const {data} = await frontendClient.get('event/all')
+            setEvents(data)
+            console.log(data)
+        }
+        fetchEvents()
+    }, [])
+    return <div className={"h-cover"}>
+        <div className="grid md:grid-cols-2 gap-10">
+            {
+                events?.map(event => <EventCard link={props.link}  key={event.id}
+                                                event={event}/>
+                )
+            }
+        </div>
+    </div>
+
+}
+
+export default EventViewer
