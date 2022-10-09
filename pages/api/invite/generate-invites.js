1dacf18e89eec1c807c2a6073cd67994fc3dd125
kazuma-0 <as8644459@gmail.com>
2022-10-09 17:05:31 +0530

    Add api endpoint for generating invites

---
+++ pages/api/invite/generate-invites.js
@@ -0,0 +1,20 @@
+import { backendClient } from "../../../client";
+
+export default async function handler(req, res) {
+    if (req.method !== "POST") {
+        res.status(401).json("Method not allowed");
+        return;
+    }
+    try {
+        const { data } = await backendClient.post(`/invite/new/${req.body.count}`, {
+        },{
+            headers:{
+                auth: req.body.auth,
+            }
+        });
+        res.status(200).json(data);
+    } catch (e) {
+        console.log(e);
+        res.status(406).json(e.response.data);
+    }
+}
