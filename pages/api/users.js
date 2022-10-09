24a2266b248191c269daba4eb4aa86cf0863ec47
kazuma-0 <as8644459@gmail.com>
2022-10-09 17:08:23 +0530

    Add api endpoint for fetching all users

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,19 +1,16 @@
-// post request is used to prevent other people from just accessing this information by logging in.
 import { backendClient } from "../../client";
 
 export default async function handler(req, res) {
-    if (req.method !== "POST") {
-        res.status(401).json("Method not allowed");
-        return;
-    }
-    try {
-        const { data } = await backendClient.get(`/auth/user/all`, {
-            headers: {
-                auth: req.body.pubKey,
-            },
-        });
-        res.status(200).json(data);
-    } catch (e) {
-        res.status(401).json("User not in club");
-    }
+  console.log(req.headers.pubKey);
+  try {
+    const { data } = await backendClient.get(`/auth/user/all`, {
+      headers: {
+        auth: req.headers.pubkey,
+      },
+    });
+    res.status(200).json(data);
+  } catch (e) {
+    console.log(e);
+    res.status(401).json("User not in club");
+  }
 }
