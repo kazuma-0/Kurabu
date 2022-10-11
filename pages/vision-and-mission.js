b2b51825ba0a78a7c0072ce923c5e8ca8453bec0
kazuma-0 <as8644459@gmail.com>
2022-10-11 16:04:09 +0530

    feat: add vision and mission

---
+++ pages/vision-and-mission.js
@@ -0,0 +1,49 @@
+function VisionMission() {
+  return (
+    <div className="h-cover py-3">
+      <h1 className="text-4xl tracking-wider font-bold font-ligurino">
+        Vision
+      </h1>
+      <p className="py-4 text-xl text-justify leading-relaxed">
+        <ul className="list-disc px-4">
+          <li>
+            Clubs for students serve as the major centre for student activities.
+            They provide youthful minds and emotions a platform and an outlet
+            for their creative energies.
+          </li>
+          <li>
+            The clubs provide you the chance to put what you learn in class to
+            use outside of the classroom. You get priceless leadership and life
+            skills as a result.
+          </li>
+          <li>
+            Students&apos; clubs allow them to communicate with alumni and
+            professionals in their fields of interest in addition to linking
+            them with other students who share those interest in AR/VR,
+            Blockchain and Data Science
+          </li>
+        </ul>
+      </p>
+      <h1 className="text-4xl tracking-wider font-bold font-ligurino">
+        Mission
+      </h1>
+      <p className="py-4 text-xl text-justify leading-relaxed">
+        <ul className="list-disc px-4">
+          <li>
+            Clubs and organizations improve your experience, foster a varied
+            community, promote participation, and offer chances for students to
+            develop important skills in AR/VR, Blockchain and Data Science. This
+            aids in the development of formal and informal networks as you look
+            into career options.
+          </li>
+          <li>
+            These groups, which are open to all students, plan activities,
+            gatherings, and events all during the academic year.
+          </li>
+        </ul>
+      </p>
+    </div>
+  );
+}
+
+export default VisionMission;
