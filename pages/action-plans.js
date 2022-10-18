97f74ce5cb055428ee69fce110f569a7036110d9
kazuma-0 <as8644459@gmail.com>
2022-10-18 09:58:57 +0530

    update action plans

--- pages/action-plans.js
+++ pages/action-plans.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
+
 function ActionPlans() {
     return (
         <>
-        <Head>
-            <title>Action Plans</title>
+            <Head>
+                <title>Action Plans</title>
             </Head>
             <div className="h-cover py-5">
                 <h1 className="text-4xl font-bold font-ligurino tracking-wider">
@@ -11,14 +12,28 @@ function ActionPlans() {
                 </h1>
                 <div className="py-3">
                     <ul className="list-disc text-xl px-5 leading-relaxed tracking-wide">
-                        <li>Planning to provide industry-oriented Hands-on Training</li>
-                        <li>Guiding students towards product-based research ideas</li>
+
+                        <li>Planning to provide industry-oriented Hands-on Training on AR/VR</li>
+
+                        <li>Guiding students toward product-based research ideas in AR/VR, Data Science and Blockchain
+                        </li>
+
+                        <li>Student Exchange Program with Kyungpook National University for Blockchain Project</li>
+
                         <li>Participation in Hackathon and other technical events</li>
-                        <li>Consultancy Project – Students are working</li>
-                        <li>Students Research paper under process for ACM SAC 2023</li>
-                        <li>Planning to conduct boot camp on AR & VR</li>
-                        <li>Planning To Conduct Two Hackathon</li>
-                        <li>Applying for Patent and Publications</li>
+
+                        <li>Consultancy Project – Students are working on a Blockchain application for an industrial
+                            framework
+                        </li>
+
+                        <li>Microsoft Student&apos;s Research paper under process for ACM SAC 2023</li>
+
+                        <li>Planning to conduct boot camp on AR &VR by SCOPIK</li>
+
+                        <li>Planning To Conduct Two Hackathons based on SDG goals</li>
+
+                        <li>Applying for Patent and Publications on specific research specializations</li>
+
                     </ul>
                 </div>
             </div>
