e6b170e5ccb1419d12b2667edff193c0ad9c6bc0
kazuma-0 <as8644459@gmail.com>
2022-10-18 09:59:10 +0530

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { Button, Center, Container } from "@chakra-ui/react";
 import metaverse from "../assets/logo.png";
+import Link from "next/link";
 function Footer() {
   return (
     <Container
@@ -9,19 +10,25 @@ function Footer() {
       className={"py-5 text-xs font-sans items-center "}
     >
       <div className="flex items-center justify-between pb-5">
-        <div>
-          <img src={metaverse.src} width="100px" alt="" />
+        <div className={"cursor-pointer"}>
+          <Link title="login"  href="/auth">
+              <img src={metaverse.src} width="100px" alt="" />
+          </Link>
+            <div className={""}>
+            </div>
         </div>
         <div className="max-w-lg text-sm space-y-1">
-          <h2 className={"text-xl tracking-wider font-bold font-sofia"}>
-            Department of Computer Science and Engineering (FOE)
+          <h2 className={"text-2xl tracking-wider font-bold font-sofia"}>
+            Faculty of Engineering
           </h2>
           <p className="text-lg">KARPAGAM ACADEMY OF HIGHER EDUCATION</p>
           <p>(Deemed to be University)</p>
           <p>
-            {/*(Established Under Section 3 of UGC Act, 1956) Pollachi Main Road,*/}
+            (Established Under Section 3 of UGC Act, 1956) Pollachi Main Road,
           </p>
-          {/*<p>Eachanari Post, Coimbatore - 641 021, Tamil Nadu, India.</p>*/}
+          <p>Eachanari Post, Coimbatore - 641 021, Tamil Nadu, India.</p>
+            <span>Contact:</span>
+            <p>contact@csemetaverse.com</p>
         </div>
       </div>
       {/* ©️{" "}
